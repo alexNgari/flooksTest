ba02dc92c7e6d998fdeffe6f1f95a7ea5b66e5cc
alexNgari <alex.ngari03@gmail.com>
2021-01-18 12:10:08 +0300

    More error handling

--- utils/files.go
+++ utils/files.go
@@ -71,7 +71,10 @@ func WriteToJSONFile(path string, borrower *models.Borrower, mutex *sync.Mutex) 
 		scores.CreditScores = append(scores.CreditScores, *creditScore)
 	}
 
-	newScoresJSON, _ := json.Marshal(scores)
+	newScoresJSON, err := json.Marshal(scores)
+	if err != nil {
+		return fmt.Errorf("Error converting results to JSON: %v", err)
+	}
 
 	ioutil.WriteFile(path, newScoresJSON, 0644)
 	return nil
